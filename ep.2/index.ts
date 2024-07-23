13b729f0e8ca101b722bf25a68c4b9fd4e517cee
anuchit <peetlove5624@gmail.com>
2024-07-23 14:45:57 +0700

    แก้

---
+++ ep.2/index.ts
@@ -0,0 +1,17 @@
+function add(a: number, b: number) {
+    return a + b
+}
+
+console.log(add(5, 8))
+
+function ex1(a: number, b: number) {
+    return a - b
+}
+
+console.log(ex1(6, 10))
+
+function ex2(a: number, b: number, c: number, d:number) {
+    return a * b * c * d
+}
+
+console.log(ex2(10, 5, 3, 2))

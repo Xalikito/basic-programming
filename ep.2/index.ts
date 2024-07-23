280cbd50421253a7255655e288e095298dd300ea
anuchit <peetlove5624@gmail.com>
2024-07-23 15:51:10 +0700

    แก้

--- ep.2/index.ts
+++ ep.2/index.ts
@@ -15,3 +15,33 @@ function ex2(a: number, b: number, c: number, d:number) {
 }
 
 console.log(ex2(10, 5, 3, 2))
+
+function grade(homework: number, midterm: number, final: number) {
+    let score: number = homework + midterm + final
+
+    if (score < 50) {
+        return 'สอบตก'
+    } else {
+        return 'สอบผ่าน'
+    }
+}
+
+console.log(grade(27, 19, 18))
+
+function grade1(homework: number, midterm: number, final: number) {
+    let score: number = homework + midterm + final
+
+    if (score < 50) {
+        return 'grade F'
+    } else if (score < 60) {
+        return 'grade D'
+    } else if (score < 70) {
+        return 'grade C'
+    } else if (score < 80) {
+        return 'grade B'
+    } else {
+        return 'grade A'
+    }
+}
+
+console.log(grade1(25, 18, 17))

91c4eb79a9b93033027e837cab332daa3da85613
Xalikito <peetlove5624@gmail.com>
2024-08-01 16:45:40 +0700

    homework bmi

--- homework/index.ts
+++ homework/index.ts
@@ -1,3 +1,15 @@
-function bmi(tall: number, weight: number) {
-    return weight / (tall * tall)
+function acceptBmi(tall: number, weight: number) {
+    let bmi: number = weight / (tall * tall)
+ if (bmi <= 18.50) {
+    return 'ผอม'
+ } else if (bmi <= 22.90) {
+    return 'ปกติ'
+ } else if (bmi <= 24.90) {
+    return 'ท้วม'
+ } else if (bmi <= 29.90) {
+    return 'อ้วน'
+ } else (bmi >= 30) ;
+    return 'อ้วนมาก'
 }
+
+console.log (acceptBmi(1.68, 40))

acf5775734df9f122d6a0ef9c1a2e8bfeb3bcb6b
Xalikito <peetlove5624@gmail.com>
2024-09-04 20:56:36 +0700

    homework

---
+++ homework/homework2.ts
@@ -0,0 +1,29 @@
+const me = {
+    firstName: 'Sompong',
+    lastName: 'Sawangja',
+    address: 15/77,
+    group: 5,
+    road: charoenKrung,
+    subdistrictSubdistrict: bangKhoLaem,
+    khetDistrict: bangKhoLaem,
+    province: bangkok,
+    postcode: 10120,
+    telephone: 191,
+    mobilePhone: Vivo,
+    email: absgg@gmail.com
+ }
+ 
+ // ประวัติส่วนตัว
+ 
+ // ชื่อ: Sompong
+ // นามสกุล: Sawangja
+ // ที่อยู่ปัจจุบันเลขที่: 15/77
+ // หมู่ที่: 5
+ // ถนน: เจริญกรุง
+ // ตำบล/แขวง: บางคอแหลม
+ // อำเภอ/เขต: บางคอแหลม
+ // จังหวัด: กรุงเทพ
+ // รหัสไปรษณีย์: 10120
+ // เบอร์โทร: 191
+ // มือถือ: vivo
+ // อีเมล์: absgg@gmail.com

788a6e37d906e2d2c0fba5e6d0c8f0df5142f4c0
Xalikito <peetlove5624@gmail.com>
2024-09-12 22:23:09 +0700

    homework

---
+++ homework/homework3.ts
@@ -0,0 +1,9 @@
+"article_id"; "9558390"
+    "title"; "สิงห์ปาร์ค เชียงราย ตั้งครัวกลางช่วยอุทกภัยใหญ่ ช่วยประชาชนและสนับสนุนการทำงานกู้ภัย"
+    "link"; "https://www.sanook.com/news/9558390/"
+    "keywords"; [
+    "น้ำท่วม"
+    ]
+    "creative"; [
+    "https://www.sanook.com/news/"
+    ]

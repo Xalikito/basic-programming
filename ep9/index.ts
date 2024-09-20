b4bf7715fefa8362ee2d29efeb70c594ecb6ac25
Xalikito <peetlove5624@gmail.com>
2024-09-20 16:36:39 +0700

    database

--- ep9/index.ts
+++ ep9/index.ts
@@ -37,3 +37,42 @@ function register(email: string, password: string) {
 }
 
 register('max@mail.com', '1234567แปด')
+
+// กฏการเข้ารหัส
+// เปลี่ยน u เป็น +
+// เปลี่ยน 6 เป็น *
+// เปลี่ยนเลข 0 เป็น ^
+
+const database = [
+    {
+        email: 'user@gmail.com',
+        password: 'loveYo+3^^^'
+    },
+    {
+        email: 'max@gmail.com',
+        password: '12345*7แปด'
+    }
+]
+
+function decryptPassword(password: string) {
+    return password.replaceAll('+', 'u').replaceAll('*', '6').replaceAll('^', '0')
+}
+
+function login(email: string, password: string) {
+    const user = database.filter(function (element, index) {
+        return element.email === email
+    })
+    if (user.length > 0) {
+        const realPassword = decryptPassword(user[0].password)
+
+        if (realPassword === password) {
+            alert('เข้าสู่ระบบสำเร็จ ยินดีต้อนรับ')
+        } else {
+            alert('รหัสผ่านไม่ถูกต้อง โปรดตรวจสอบ')
+        }
+    } else {
+        alert('ไม่พบผู้ใช้งานในระบบ')
+    }
+}
+
+login('max@gmail.com', '1234567แปด')
